25794136ebb18da625548a34b966a4b9783407e7
gitblair <gitblair@users.noreply.github.com>
2022-06-21 12:43:06 -0400

    removed alerts. focusing on weathers

--- display-weather.php
+++
@@ -1,8 +0,0 @@
-<?php
-include_once 'pull-alerts-and-weathers.php';
-include_once 'create-alerts-and-weathers.php';
-
-echo $weathers;
-echo $alerts;
-
-?>
---
+++ display-weathers.php
@@ -0,0 +1,6 @@
+<?php
+include_once 'pull-weathers.php';
+include_once 'create-weathers.php';
+
+echo $weathers;
+?>
